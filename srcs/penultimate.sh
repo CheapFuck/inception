0bc403d10621b22c7fd740f22246ca241b7c807c
Thijs Van Den Berg <thivan-d@f1r4s20.codam.nl>
2025-04-08 15:13:40 +0200

    penultimate

--- srcs/penultimate.sh
+++ srcs/penultimate.sh
@@ -27,6 +27,6 @@ wget -q $url -O alpine-minirootfs-${penultimate_version}-x86_64.tar.gz
 # Output the downloaded penultimate version
 echo "Downloaded Alpine penultimate version: ${penultimate_version}"
 sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/nginx/Dockerfile
-# sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/mariadb/Dockerfile
+sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/mariadb/Dockerfile
 
 # cat Dockerfile

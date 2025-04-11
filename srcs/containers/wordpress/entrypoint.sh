2aa04df36a5b383bb7163f725bc342dfc44598a8
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-04-11 17:03:33 +0200

    sfsaf

--- srcs/containers/wordpress/entrypoint.sh
+++ srcs/containers/wordpress/entrypoint.sh
@@ -4,10 +4,6 @@
 if [ ! -f /var/www/html/wp-config.php ]; then
   cp /tmp/wp-config.php /var/www/html/wp-config.php
 fi
-until nc -z mariadb 3306; do
-  echo "Waiting for MariaDB to be ready..."
-  sleep 2
-done
-echo "MariaDB is ready. Starting WordPress..."
+
 # Then run PHP-FPM or whatever your CMD is
 exec php-fpm83 -F

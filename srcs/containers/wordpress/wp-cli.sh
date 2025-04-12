66fb084c69c4c44e657e5af04e7ad8c408a16020
Thijs Van Den Berg <thivan-d@f1r3s12.codam.nl>
2025-04-12 16:12:19 +0200

    adsad

--- srcs/containers/wordpress/wp-cli.sh
+++ srcs/containers/wordpress/wp-cli.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # Wait for MariaDB to be ready
 until mariadb -h mariadb -u my_user -pmy_password -e "SHOW DATABASES;" > /dev/null 2>&1; do
   echo "Waiting for MariaDB to be ready..."
@@ -7,30 +6,34 @@ until mariadb -h mariadb -u my_user -pmy_password -e "SHOW DATABASES;" > /dev/nu
 done
 
 echo "MariaDB is ready. Continuing WordPress setup..."
-
-# Install wp-cli if missing
+# Check if wp-cli is installed, if not, install it
 if ! command -v wp > /dev/null 2>&1; then
-  echo "Installing wp-cli..."
+  echo "wp-cli is not installed. Installing wp-cli..."
   curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
+  echo "wp-cli installed successfully."
 fi
 
-# Ensure /var/www/html/wordpress exists
-mkdir -p /var/www/html/wordpress
+# Check if the "/var/www/html" folder exists, if not create it
+if [ ! -d "/var/www/html/wordpress" ]; then
+  mkdir -p /var/www/html/wordpress
+fi
+
+# Change to the WordPress directory
 cd /var/www/html/wordpress
 
-# Download WordPress core
+
+# This downloads the WordPress core files, the option ( --allow-root ) will run the command as root
+# and ( --version:5.8.1 ) specifies the version of WordPress that will get downloaded
+# and ( --local=en_US ) sets the language of the installation to US English
 wp core download --allow-root --version=6.7.2 --locale=en_US
 
-# Create wp-config
-wp config create --allow-root \
-  --dbname="my_database" \
-  --dbuser="my_user" \
-  --dbpass="my_password" \
-  --dbhost="mariadb:3306"
+# This will generate the WordPress configuration file, and the options ( --dbname, --dbuser, --dbpass, --dbhost )
+# are just placeholders that will get replaced once the script runs
+wp config create --allow-root --dbname="my_database" --dbuser="my_user" --dbpass="my_password" --dbhost="mariadb:3306"
 
-# Install WordPress
+# This will then install WordPress, and again, all the options are just placeholders that will get replaced
 wp core install --allow-root \
   --url="https://localhost/wordpress" \
   --title="My WordPress Site" \
@@ -38,9 +41,8 @@ wp core install --allow-root \
   --admin_password="1d0ntkn0w!?" \
   --admin_email="thivan-d@student.codam.nl"
 
-# Create additional WordPress user
-wp user create "my_user" "thivan-d@student.codam.nl" \
-  --user_pass="my_password" --role=author
+# This create a new WordPress user, and sets its role to author ( --role=author )
+wp user create "my_user" "thivan-d@student.codam.nl" --user_pass="my_password" --role=author
 
-# Start PHP-FPM
+# This is the command that will keep WordPress up and running
 exec php-fpm83 -F -R

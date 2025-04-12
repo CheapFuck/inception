e43fda453f391495e42af84e7729aaf2be802f62
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-04-12 14:08:27 +0200

    assad

--- srcs/containers/mariadb/init.sql
+++ srcs/containers/mariadb/init.sql
@@ -1,5 +1,5 @@
 -- Create a database and a user with privileges
 CREATE DATABASE IF NOT EXISTS my_database;
 CREATE USER IF NOT EXISTS 'my_user'@'%' IDENTIFIED BY 'my_password';
-GRANT ALL PRIVILEGES ON my_database.* TO 'my_user'@'%';
+GRANT ALL PRIVILEGES ON my_database.* TO 'my_user'@'%' IDENTIFIED BY 'my_password';
 FLUSH PRIVILEGES;

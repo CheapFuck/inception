71a364a047306bd09b3ab5ace8cd3eb9ccb3ed21
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-04-21 14:48:07 +0200

    oaijfjf

--- srcs/containers/wordpress/entrypoint.sh
+++ srcs/containers/wordpress/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # If the WordPress directory is empty, download and extract WordPress
-if [ ! "$(ls -A /var/www/html)" ]; then
+if [ ! "$(ls -A /var/www/html/wordpress)" ]; then
   echo "WordPress is not installed, installing..."
   curl -O https://wordpress.org/latest.tar.gz && \
-  tar -xzf latest.tar.gz -C /var/www/html --strip-components=1 && \
+  tar -xzf latest.tar.gz -C /var/www/html/wordpress --strip-components=1 && \
   rm latest.tar.gz
 else
   echo "WordPress already installed, skipping installation."

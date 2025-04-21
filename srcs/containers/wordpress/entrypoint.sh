46791a074819457275e34664581657ecbb07bb13
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-04-21 15:02:06 +0200

    bladiebla

--- srcs/containers/wordpress/entrypoint.sh
+++ srcs/containers/wordpress/entrypoint.sh
@@ -3,6 +3,7 @@
 # If the WordPress directory is empty, download and extract WordPress
 if [ ! "$(ls -A /var/www/html/wordpress)" ]; then
   echo "WordPress is not installed, installing..."
+  cp /index.html /var/www/html/index.html
   mkdir -p /var/www/html/wordpress
   curl -O https://wordpress.org/latest.tar.gz && \
   tar -xzf latest.tar.gz -C /var/www/html/wordpress --strip-components=1 && \

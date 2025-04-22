3c82f59264118e53a55ada769441dfc8c10d1d00
Thijs Van Den Berg <thivan-d@f1r4s16.codam.nl>
2025-04-22 19:52:24 +0200

    lalahoimiauw

--- srcs/containers/wordpress/entrypoint.sh
+++ srcs/containers/wordpress/entrypoint.sh
@@ -10,18 +10,30 @@ if [ ! `find /var/www/html/wordpress -name wp-config.php` ]; then
 #   tar -xzf latest.tar.gz -C /var/www/html/wordpress --strip-components=1 && \
 #   rm latest.tar.gz
   
-  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar 
-  mv wp-cli.phar /usr/local/bin/wp
+  # wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  # chmod +x wp-cli.phar 
+  # mv wp-cli.phar /usr/local/bin/wp
   cd /var/www/html/wordpress
   wp core download --allow-root
   wp core config --dbhost="mariadb":"3306" --dbname="my_database" --dbuser="my_user" --dbpass="my_password" --allow-root
-  wp core install --url="thivan-d.42.fr" --title="sitymcsiteface" --admin_user="bawse" --admin_password="wafwaf" --admin_email="thivan-d@student.codam.nl" --allow-root
+  wp core install --url="localhost/wordpress/" --title="sitymcsiteface" --admin_user="bawse" --admin_password="wafwaf" --admin_email="thivan-d@student.codam.nl" --allow-root
+  until wp db check --allow-root --path=/var/www/html/wordpress; do
+    echo "Waiting for database..."
+    sleep 2
+  done
   wp user create "miauw" "thivan-d@student.codam.nl" --user_pass="waf" --allow-root
 else
   echo "WordPress already installed, skipping installation."
 fi
-# cp /wp-config.php /var/www/html/wordpress/wp-config.php
+cp /wp-config.php /var/www/html/wordpress/wp-config.php
+# Create user "miauw" if it doesn't exist
+echo "Checking if 'miauw' exists..."
+if ! wp user get miauw --allow-root --path=/var/www/html/wordpress > /dev/null 2>&1; then
+  echo "'miauw' not found. Creating user..."
+  wp user create "miauw" "thiiiiivan-d@student.codam.nl" --user_pass="waf" --role=subscriber --allow-root --path=/var/www/html/wordpress
+else
+  echo "User 'miauw' already exists."
+fi
 chown -R nobody:nobody *
 # Start PHP-FPM
 exec php-fpm83 -F -R

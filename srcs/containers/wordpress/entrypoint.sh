b6584a1fc6095d7e3ca7350584497efc09aa3e98
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-04-24 12:15:40 +0200

    lala

--- srcs/containers/wordpress/entrypoint.sh
+++ srcs/containers/wordpress/entrypoint.sh
@@ -2,45 +2,45 @@
 
 # If the WordPress directory is empty, download and extract WordPress
 # if [ ! "$(ls -A /var/www/html/wordpress)" ]; then
-if [ ! `find /var/www/html/wordpress -name wp-config.php` ]; then
-  echo "WordPress is not installed, installing..."
-  cp /index.html /var/www/html/index.html
-  mkdir -p /var/www/html/wordpress
-#   curl -O https://wordpress.org/latest.tar.gz && \
-#   tar -xzf latest.tar.gz -C /var/www/html/wordpress --strip-components=1 && \
-#   rm latest.tar.gz
+# if [ ! `find /var/www/html/wordpress -name wp-config.php` ]; then
+#   echo "WordPress is not installed, installing..."
+#   cp /index.html /var/www/html/index.html
+#   mkdir -p /var/www/html/wordpress
+# #   curl -O https://wordpress.org/latest.tar.gz && \
+# #   tar -xzf latest.tar.gz -C /var/www/html/wordpress --strip-components=1 && \
+# #   rm latest.tar.gz
   
-  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar 
-  mv wp-cli.phar /usr/local/bin/wp
-  wp --info --allow-root
-  cd /var/www/html/wordpress
-  chown -R nobody:nobody *
-  wp core download --allow-root
-  # chown -R nobody:nobody *
-  wp core config --dbhost="mariadb":"3306" --dbname="my_database" --dbuser="my_user" --dbpass="my_password" --allow-root
-  wp core install --title="sitymcsiteface" --admin_user="bawse" --admin_password="wafwaf" --admin_email="thivan-d@student.codam.nl" --allow-root
-  until wp db check --allow-root --path=/var/www/html/wordpress; do
-    echo "Waiting for database..."
-    sleep 2
-  done
-  wp user create "miauw" "thivan-d@student.codam.nl" --user_pass="waf" --allow-root
-else
-  echo "WordPress already installed, skipping installation."
-fi
-cp /wp-config.php /var/www/html/wordpress/wp-config.php
-wp plugin install redis-cache --activate
-
-# Create user "miauw" if it doesn't exist
-# echo "Checking if 'miauw' exists..."
-# if ! wp user get miauw --allow-root --path=/var/www/html/wordpress > /dev/null 2>&1; then
-#   echo "'miauw' not found. Creating user..."
-#   wp user create "miauw" "thiiiiivan-d@student.codam.nl" --user_pass="waf" --role=subscriber --allow-root --path=/var/www/html/wordpress
+#   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#   chmod +x wp-cli.phar 
+#   mv wp-cli.phar /usr/local/bin/wp
+#   wp --info --allow-root
+#   cd /var/www/html/wordpress
+#   chown -R nobody:nobody *
+#   wp core download --allow-root
+#   # chown -R nobody:nobody *
+#   wp core config --dbhost="mariadb":"3306" --dbname="my_database" --dbuser="my_user" --dbpass="my_password" --allow-root
+#   wp core install --title="sitymcsiteface" --admin_user="bawse" --admin_password="wafwaf" --admin_email="thivan-d@student.codam.nl" --allow-root
+#   until wp db check --allow-root --path=/var/www/html/wordpress; do
+#     echo "Waiting for database..."
+#     sleep 2
+#   done
+#   wp user create "miauw" "thivan-d@student.codam.nl" --user_pass="waf" --allow-root
 # else
-#   echo "User 'miauw' already exists."
+#   echo "WordPress already installed, skipping installation."
 # fi
-# Start PHP-FPM
-exec php-fpm83 -F -R
+# cp /wp-config.php /var/www/html/wordpress/wp-config.php
+# wp plugin install redis-cache --activate
+
+# # Create user "miauw" if it doesn't exist
+# # echo "Checking if 'miauw' exists..."
+# # if ! wp user get miauw --allow-root --path=/var/www/html/wordpress > /dev/null 2>&1; then
+# #   echo "'miauw' not found. Creating user..."
+# #   wp user create "miauw" "thiiiiivan-d@student.codam.nl" --user_pass="waf" --role=subscriber --allow-root --path=/var/www/html/wordpress
+# # else
+# #   echo "User 'miauw' already exists."
+# # fi
+# # Start PHP-FPM
+# exec php-fpm83 -F -R
 
 
 # --url="localhost/wordpress/"

25d080e68f927f1bf0e966e328fa3c083ee7e015
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-04-25 15:32:26 +0200

    lala

--- srcs/containers/wordpress/entrypoint.sh
+++ srcs/containers/wordpress/entrypoint.sh
@@ -30,7 +30,8 @@ else
 fi
 
 cp /wp-config.php /var/www/html/wordpress/wp-config.php
-# wp plugin install redis-cache --activate --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp redis enable --allow-root
 # wp option update wp_redis_cache_enabled 1 --allow-root
 
 # wp plugin install redis-cache --activate

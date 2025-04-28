2fbb83ce934e70d630f230f9a421e0f644f0e522
Thijs Van Den Berg <thivan-d@f1r3s3.codam.nl>
2025-04-28 14:07:44 +0200

    lala

--- srcs/containers/mariadb/entrypoint.sh
+++ srcs/containers/mariadb/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-mariadbd --user=mysql --bind-address=0.0.0.0 --port=3306 --init-file=/init.sql"
+mariadbd --user=mysql --bind-address=0.0.0.0 --port=3306 --init-file=/init.sql

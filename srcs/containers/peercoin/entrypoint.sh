da5581c0b56600054f2d107bbf2d1377d8e657e8
Thijs Van Den Berg <thivan-d@f1r3s17.codam.nl>
2025-04-29 14:07:04 +0200

    lala

--- srcs/containers/peercoin/entrypoint.sh
+++ srcs/containers/peercoin/entrypoint.sh
@@ -32,7 +32,8 @@ if [ "$1" = "peercoind" ] || [ "$1" = "peercoin-cli" ] || [ "$1" = "peercoin-tx"
   echo
   exec su-exec peercoin "$@"
 fi
-cd peercoin
+# cd peercoin
+export PATH="/peercoin:$PATH"
 exec "$@"
 
 

d7b29eb2cb2d0297dacd002dbdb1308ad4a5d816
Thijs Van Den Berg <thivan-d@f1r3s17.codam.nl>
2025-04-30 15:23:31 +0200

    lala

--- srcs/containers/wordpress/wait.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# wait for the database to be available
-until mariadb-admin ping -h"mariadb" -u"my_user" -p"my_password" --silent; do
-  >&2 echo "MariaDB is unavailable - sleeping"
-  sleep 5
-done
-
-# continue with your wp-cli commands

6bcc3a8ba075c6ac86707ffa026b770c4c6a7a25
Thijs Van Den Berg <thivan-d@f1r3s11.codam.nl>
2025-05-01 16:30:16 +0200

    ad

--- srcs/containers/wordpress/entrypoint.sh
+++ srcs/containers/wordpress/entrypoint.sh
@@ -14,8 +14,8 @@ if [ ! `find /var/www/html/wordpress -name wp-config.php` ]; then
   wp core download --allow-root
   cp /wp-config.php /var/www/html/wordpress/wp-config.php
   chown -R nobody:nobody *
-  wp core install --url="http://thivan-d.42.nl/wordpress" --title="sitymcsiteface" --admin_user="bawse" --admin_password="wafwaf" --admin_email="thivan-d@student.codam.nl" --allow-root
-  wp user create "miauw" "thssivan-d@student.codam.nl" --user_pass="waf" --allow-root
+  wp core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIN_PASS}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
+  wp user create "${WP_USER}" "${WP_EMAIL}" --user_pass="${WP_USER_PASS}" --allow-root
   wp plugin activate hello --allow-root
   wp plugin install redis-cache --activate --allow-root
   wp redis enable --allow-root

9d032bc77e939c89d089da7a75c7287dbcb88351
Thijs Van Den Berg <thivan-d@f1r3s17.codam.nl>
2025-05-02 15:21:02 +0200

    asd

--- srcs/containers/wordpress/wp-config.php
+++ srcs/containers/wordpress/wp-config.php
@@ -19,21 +19,19 @@
  */
 
 
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
+ 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $_ENV['WORDPRESS_DB_NAME'] );
+define( 'DB_NAME', 'my_database' );
+
 /** Database username */
-define( 'DB_USER', $_ENV['WORDPRESS_DB_USER'] );
+define( 'DB_USER', 'my_user' );
+
 /** Database password */
-define( 'DB_PASSWORD', $_ENV['WORDPRESS_DB_PASSWORD'] );
+define( 'DB_PASSWORD', 'my_password' );
+
 /** Database hostname */
-define( 'DB_HOST', $_ENV['WORDPRESS_DB_HOST'] );
+define( 'DB_HOST', 'mariadb:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
@@ -89,11 +87,11 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', false );
-// define('WP_DEBUG', true);
+// define( 'WP_DEBUG', false );
+define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);  // Hides errors from the frontend, logs them instead
-
+// define('WP_DEBUG_DISPLAY', false);  // Hides errors from the frontend, logs them instead
+define('WP_DEBUG_DISPLAY', true);  // Hides errors from the frontend, logs them instead
 
 
 

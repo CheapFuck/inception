6b42fa9cac79258a172de3a56d6bdf61ec2be728
Thijs Van Den Berg <thivan-d@f1r3s17.codam.nl>
2025-05-02 14:47:37 +0200

    sad

--- srcs/containers/wordpress/wp-config.php
+++ srcs/containers/wordpress/wp-config.php
@@ -57,20 +57,62 @@ define( 'SECURE_AUTH_SALT', 'qI-JwvUZ&xOu@?Uv3&:h)T+6U>[g?,xr*5ib)%eim<b}))e~--+
 define( 'LOGGED_IN_SALT',   '3{Tl-(<Ae-#o)9)tUD/,C0e$R!2z(q:m?o4`q*X4#x.[:j-06BGf%-HVc^Rq9$Ba' );
 define( 'NONCE_SALT',       'j#{;&/Dzn;,];%*F(MN=byR$`j1F`N>KJ.-;p-d#;;9cC`+a;(L2${m^f!Lz:wzt' );
 
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
 $table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
 define( 'WP_DEBUG', false );
 // define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_DISPLAY', false);  // Hides errors from the frontend, logs them instead
+
+
+
+
+
+/* Add any custom values between this line and the "stop editing" line. */
+
 // Redis Cache Settings
-define( 'WP_REDIS_HOST', 'redis' );
-define( 'WP_REDIS_PORT', 6379 );
+define( 'WP_REDIS_HOST', 'redis' );  // The Redis service name defined in docker-compose.yml
+define( 'WP_REDIS_PORT', 6379 );     // The port Redis is running on (default is 6379)
+// define( 'WP_REDIS_DATABASE', 0 );    // Default Redis database is 0 (optional, use if needed)
+// define( 'WP_REDIS_DISABLED', false );
 define( 'WP_CONTENT_DIR', '/var/www/html/wordpress/wp-content' );
-define('FS_METHOD', 'direct');
+define('FS_METHOD', 'direct');  // Tells WordPress to use direct file access
+// define('WP_REDIS_ENABLED', true); // Enable Redis Object Cache
+//define( 'WP_CONTENT_URL', 'http://yourdomain.com/wordpress/wp-content' );
+
+
 /* That's all, stop editing! Happy publishing. */
+
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
+
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';

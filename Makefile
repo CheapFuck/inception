14d2425dc7fa0abe5c8685819c60b0cb2c6ba72b
Thijs Van Den Berg <thivan-d@f1r4s1.codam.nl>
2025-05-04 13:35:39 +0200

    lala

--- Makefile
+++ Makefile
@@ -1,13 +1,11 @@
 .PHONY: run_script all up down stop start create_dirs
 
-# Directories you want to check/create
 DIRS := $(HOME)/data/peercoin $(HOME)/data/mariadb
+USER_NAME=$(shell whoami)
 
-# Ensure that 'run_script' and 'create_dirs' are part of the 'all' target
 all: create_dirs update-yml run_script up
 	@echo "all target executed"
 
-# Target to check and create directories
 create_dirs:
 	@for dir in $(DIRS); do \
 		if [ ! -d $$dir ]; then \
@@ -18,36 +16,23 @@ create_dirs:
 		fi \
 	done
 
-YML_FILE=srcs/docker-compose.yml
-
-# Run your script
 run_script:
 	./srcs/penultimate.sh
-
-# Get the current username
-USER_NAME=$(shell whoami)
-
-# yml
-yml_file=srcs/docker-compose.yml
 
 update-yml:
 	@echo "Updating YML file with current username..."
 	@sed -i "s|/home/___USERNAME___|/home/$(USER_NAME)|g" srcs/docker-compose.yml
 	@echo "YML file updated successfully!"
 
-# Docker up (builds and starts containers)
 up:
 	@echo "up target executed"
 	@docker compose -f srcs/docker-compose.yml up --build -d
 
-# Docker down (stops and removes containers)
 down:
 	@docker compose -f srcs/docker-compose.yml down
 
-# Docker stop (stops containers)
 stop:
 	@docker compose -f srcs/docker-compose.yml stop
 
-# Docker start (starts containers)
 start:
 	@docker compose -f srcs/docker-compose.yml start
--- srcs/penultimate.sh
+++ srcs/penultimate.sh
@@ -1,39 +1,19 @@
 #!/bin/bash
 
-# Fetch the list of Alpine versions from the official Alpine releases page for the latest stable release series
 versions=$(curl -s https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/ | grep -oP 'alpine-minirootfs-\K[0-9\.]+(?=-x86_64.tar.gz)' | sort -V | uniq)
-
-# Debug: Output the list of versions to check if there are any duplicates
 echo "Available versions:"
 echo "$versions"
-
-# Get the latest version  (last in the sorted list)
 latest_version=$(echo "$versions" | tail -n 1)
-
-# Get the penultimate version (second-to-last in the sorted list)
 penultimate_version=$(echo "$versions" | tail -n 2 | head -n 1)
-
-# Debug: Output the latest and penultimate versions to verify correctness
 echo "Latest version: ${latest_version}"
 echo "Penultimate version: ${penultimate_version}"
-
-# Set the URL to download the penultimate tarball
 url="https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-${penultimate_version}-x86_64.tar.gz"
-
-# Download the tarball
-# pwd
-# wget -q $url -O srcs/alpine-minirootfs-${penultimate_version}-x86_64.tar.gz
 filename="srcs/alpine-minirootfs-${penultimate_version}-x86_64.tar.gz"
-
 if [ -f "$filename" ]; then
     echo "File $filename already exists. Skipping download."
 else
     wget -q "$url" -O "$filename"
 fi
-
-
-
-# Output the downloaded penultimate version
 echo "Downloaded Alpine penultimate version: ${penultimate_version}"
 sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/nginx/Dockerfile
 sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/mariadb/Dockerfile
@@ -42,11 +22,3 @@ sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimat
 sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/redis/Dockerfile
 sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/adminer/Dockerfile
 sed -i "s|alpine-minirootfs-[0-9.]*-x86_64.tar.gz|alpine-minirootfs-${penultimate_version}-x86_64.tar.gz|" srcs/containers/peercoin/Dockerfile
-
-
-
-
-# cat srcs/containers/nginx/Dockerfile
-# cat srcs/containers/mariadb/Dockerfile
-
-# cat Dockerfile
